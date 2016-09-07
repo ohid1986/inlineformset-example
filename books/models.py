2477b1ce07b4ca17f51289cd1017eaa3ac5d0e5c
Md. Ohiduzzaman <ohid1986@gmail.com>
2016-09-07 12:46:57 +0600

    Python 3.5 compability issues fixed. Changes __unicode__ to __str__

--- books/models.py
+++ books/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
-    @models.permalink
+
     def get_absolute_url(self):
         return ('author_detail', [self.pk])
 
@@ -16,9 +16,9 @@ class Book(models.Model):
     author = models.ForeignKey(Author)
     title = models.CharField(max_length=100)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
 
-    @models.permalink
+
     def get_absolute_url(self):
         return ('book_detail', [self.pk])
